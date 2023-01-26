07c73c90917affc6fdbb76c73b4db0a6ec2f490a
Robert Pliszka <robplisz@gmail.com>
2023-01-26 18:14:30 -0500

    Maded Changes to Components to pass in props instead of having own objects

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,40 @@
-import React from "react";
-import logo from "../../logo.svg";
-import "./App.css";
-import BusinessList from "../BusinessList/BusinessList";
-import SearchBar from "../SearchBar/SearchBar";
-import { render } from "@testing-library/react";
+import React from 'react';
+import './App.css';
 
-export default class App extends React.Component {
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Bordertown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
 }
+
+export default App;
--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,41 +1,30 @@
 import React from "react";
 import "./Business.css";
 
-// Temporary business data as temporary object
-const business = {
-  imageSrc:
-    "https://www.spellbrite.com/wp-content/uploads/PIZZA-1-neon-led-custom-sign-changable-letters1.jpg",
-  name: "MarginOtto Pizzeria",
-  address: "1010 Paddington Way",
-  city: "Flavortown",
-  state: "NY",
-  zipCode: "10101",
-  category: "Italian",
-  rating: 4.5,
-  reviewCount: 90,
-};
-
 // Business component
 class Business extends React.Component {
   render() {
     return (
       <div className="Business">
         <div className="image-container">
-          <img src={business.imageSrc} alt={business.name} />
+          <img
+            src={this.props.business.imageSrc}
+            alt={this.props.business.name}
+          />
         </div>
-        <h2>{business.name}</h2>
+        <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
-            <p>{business.address}</p>
-            <p>{business.city}</p>
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
             <p>
-              {business.state} {business.zipCode}
+              {this.props.business.state} {this.props.business.zipCode}
             </p>
           </div>
           <div className="Business-reviews">
-            <h3>{business.category}</h3>
-            <h3 className="rating">{business.category}</h3>
-            <p>{business.reviewCount} reviews</p>
+            <h3>{this.props.business.category}</h3>
+            <h3 className="rating">{this.props.business.category}</h3>
+            <p>{this.props.business.reviewCount} reviews</p>
           </div>
         </div>
       </div>
