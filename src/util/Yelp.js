f5c81307686555a1eca364b5863787580140134c
Robert Pliszka <robplisz@gmail.com>
2023-02-04 14:54:58 -0500

    Added fetch code to the Yelp.js component

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,9 +1,40 @@
 // Api Key for the Yelp API access.
-const apiKey =
-  "";
+const apiKey = "";
 
+//   Stores information about Yelp request
 const Yelp = {
   search(term, location, sortBy) {
-    return fetch(`https://api.yelp.com/v3/business/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+    // fetch request
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/business/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.business) {
+          return jsonResponse.business.map((business) => {
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.address,
+              city: business.city,
+              state: business.state,
+              zipCode: business.zip_code,
+              category: business.category,
+              rating: business.rating,
+              reviewCount: business.review_count,
+            };
+          });
+        }
+      });
   },
 };
+
+export default Yelp;
